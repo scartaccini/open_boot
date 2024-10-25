5c6ababb98d1817a9f3eb92737c2613dbaee9d9a
scartaccini <pablo.scartaccini@gmail.com>
2024-10-25 12:11:12 -0300

    modifcacion en models

--- proyecto_grafitis/sistema_gestion_grafitis/models.py
+++ proyecto_grafitis/sistema_gestion_grafitis/models.py
@@ -12,7 +12,7 @@ class Grafiti(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, blank=False, null=False)
     direccion = models.CharField(max_length=200, blank=False, null=False)
-    imagen=models.ImageField(upload_to='fotos', blank=True, null=True)
+    imagen=models.ImageField(upload_to='fotos', blank=False, null=False)
     #artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
 
     def __str__(self):
